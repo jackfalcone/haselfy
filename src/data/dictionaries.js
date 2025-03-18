7921e0ffd17e0247fdb3d3a9f7be7b1993b432e0
yvesd <yves.daetwyler@gmail.com>
2025-03-18 12:15:28 +0100

    add further words to dict

--- src/data/dictionaries.js
+++ src/data/dictionaries.js
@@ -8,7 +8,7 @@ export const dictionaries = [
         dict: [
             'Arztzimmer', 'Hauptgebäude', 'GR', 'Matterhorn', 'Schulungsraum', 'ARTelier',
             'Auditorium_Go', 'Haupteingang', 'Ergotherapieraum', 'Fitnessraum', 'Sitzungszimmer',
-            'Venus', 'Soz.-Dienstzimmer', 'TZ', '22', 'Eingangshalle'
+            'Venus', 'Soz.-Dienstzimmer', 'TZ', '22', 'Eingangshalle', 'Küche'
           ],
         threshold: 0.85,
         type: 'locations',
@@ -20,7 +20,8 @@ export const dictionaries = [
             'Autilio', 'Breit', 'Brömmer', 'Filippo', 'Koch',
             'Hierzer', 'Köse',  'Kusluk', 'Müller-Welti', 'Beer',
             'Guercio', 'Holenweg', 'Imboden', 'Mahendran', 'Scheumann',
-            'Widmer', 'Mallien'
+            'Widmer', 'Mallien', 'Suter', 'Scheurmann', 'Friedli',
+            'Baranowska', 'Germann', 'Oeggerli', 'Erni'
           ],
         threshold: 0.55,
         type: 'organizers',
@@ -33,7 +34,10 @@ export const dictionaries = [
             'Psychoed.', 'Sucht', 'RPT', 'Einzel', 'Sozialdienst', 'Willkommensrunde',
             'Ergotherapie', 'S.T.A.R.K.', 'Erstgespräch', 'Aufnahme', 'Ärztl.',
             'Patientenadministration', 'Abendessen', 'mit', 'Götti', 'Sporttherapie',
-            'Kunsttherapie', 'Begleitung', 'Haushaltstraining'
+            'Kunsttherapie', 'Themenzentriertes', 'Gestalten', 'Begleitung', 
+            'Haushaltstraining', 'Cogpack', 'Küchenkleider', 'in', 'Wäscherei', 'abholen',
+            'Ergo', 'Küche', 'Erholung', 'Natur', 'kreativer', 'Ausdruck',
+            'Recoverygruppe'
           ],
         threshold: 0.8,
         type: 'expectedWords',
